f824cc305ebcd0687bff627050c01e01acb81dcb
Kishor Irnak <kishor@example.com>
2026-02-21 00:46:19 +0530

    Fix github actions workflow and Next config for GitHub pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/campanion",
-  assetPrefix: "/campanion",
+  basePath: isGithubActions ? "/campanion" : "",
+  assetPrefix: isGithubActions ? "/campanion" : "",
 };
 
 export default nextConfig;

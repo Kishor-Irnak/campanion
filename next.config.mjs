9775eecdc9629b23490bf1ecb0432956ee526d56
Kishor Irnak <kishor@example.com>
2026-02-21 00:27:43 +0530

    Fix local environment routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/campanion",
-  assetPrefix: "/campanion",
+  basePath: isProd ? "/campanion" : "",
+  assetPrefix: isProd ? "/campanion" : "",
 };
 
 export default nextConfig;
